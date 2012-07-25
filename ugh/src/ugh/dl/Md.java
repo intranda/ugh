c770c11b5177554d71e23ed81170ac979dce0374
Robert Sehr <Robert.Sehr@intranda.com>
2012-07-25 10:38:08 +0200

    added licence information to new classes

--- ugh/src/ugh/dl/Md.java
+++ ugh/src/ugh/dl/Md.java
@@ -1,5 +1,26 @@
 package ugh.dl;
-
+/*******************************************************************************
+ * ugh.dl / MD.java
+ * 
+ * Copyright 2012 intranda GmbH, Göttingen
+ * 
+ * http://www.intranda.com
+ * http://www.digiverso.com
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This Library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ ******************************************************************************/
 import java.io.Serializable;
 
 import org.w3c.dom.Node;
