52977f0f261efb6a9d27dd8f91fdbdb9bb26c5d2
Robert Sehr <robert.sehr@intranda.com>
2014-07-03 09:20:46 +0200

    update to mods 3.5

--- ugh/src/ugh/fileformats/mets/MetsMods.java
+++ ugh/src/ugh/fileformats/mets/MetsMods.java
@@ -369,7 +369,7 @@ public class MetsMods implements ugh.dl.Fileformat {
     private static final String DEFAULT_METS_SCHEMA_LOCATION = "http://www.loc.gov/standards/mets/version17/mets.v1-7.xsd";
     private static final String DEFAULT_MODS_PREFIX = "mods";
     private static final String DEFAULT_MODS_URI = "http://www.loc.gov/mods/v3";
-    private static final String DEFAULT_SCHEMA_LOCATION = "http://www.loc.gov/standards/mods/v3/mods-3-3.xsd";
+    private static final String DEFAULT_SCHEMA_LOCATION = "http://www.loc.gov/standards/mods/v3/mods-3-5.xsd";
     private static final String DEFAULT_GOOBI_PREFIX = "goobi";
     private static final String DEFAULT_GOOBI_URI = "http://meta.goobi.org/v1.5.1/";
     private static final String DEFAULT_GOOBI_SCHEMA_LOCATION = "";
