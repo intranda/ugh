74a78a0b8d5c5018f55e9f9adb86dd0b794501e0
zehong <zehong.hu@intranda.com>
2023-02-20 17:54:51 +0100

    unit tests for ugh.fileformats.slimjson.SlimContentFileReference

---
+++ ugh/src/test/java/ugh/fileformats/slimjson/SlimContentFileReferenceTest.java
@@ -0,0 +1,41 @@
+package ugh.fileformats.slimjson;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import ugh.dl.ContentFile;
+import ugh.dl.ContentFileArea;
+import ugh.dl.ContentFileReference;
+
+public class SlimContentFileReferenceTest {
+
+    @Ignore("The logic in the method cannot pass this test. Null check needed for the first parameter to avoid the NullPointerException.")
+    @Test
+    public void testFromContentFileReferenceGivenNullAsFirstParameter() {
+        SlimDigitalDocument sdd = new SlimDigitalDocument();
+        assertNull(SlimContentFileReference.fromContentFileReference(null, sdd));
+    }
+
+    @Ignore("The logic in the method cannot pass this test. Null check needed for the second parameter to avoid the NullPointerException.")
+    @Test
+    public void testFromContentFileReferenceGivenNullAsSecondParameter() {
+        ContentFileReference cfr = new ContentFileReference();
+        // before initializing the fields of cfr
+        assertNull(cfr.getCf());
+        assertNull(cfr.getCfa());
+        assertNull(SlimContentFileReference.fromContentFileReference(cfr, null));
+
+        // initialize both fields of cfr and test this method again
+        ContentFile cf = new ContentFile();
+        ContentFileArea cfa = new ContentFileArea();
+        cfr.setCf(cf);
+        cfr.setCfa(cfa);
+        assertNotNull(cfr.getCf());
+        assertNotNull(cfr.getCfa());
+        assertNull(SlimContentFileReference.fromContentFileReference(cfr, null));
+    }
+
+}
